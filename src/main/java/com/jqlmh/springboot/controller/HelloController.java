05b396c090fdd4494628e3a5ee225b995f6965f0
lmh_glb <jq_glb961013@lmh.com>
2020-03-13 16:32:10 +0800

    第二次修改了hello控制器的,返回值

--- src/main/java/com/jqlmh/springboot/controller/HelloController.java
+++ src/main/java/com/jqlmh/springboot/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(Model model){
-        model.addAttribute("msg","小卢爱姜覃~~~~~❤");
+        model.addAttribute("msg","小卢爱姜覃~~~~~❤么么哒");
         return "success";
     }
 }
